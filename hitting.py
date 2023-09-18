d85b469e31e86ca7dcad34615fa7ae132b0ec9a1
Shobhit1045 <79567383+Shobhit1045@users.noreply.github.com>
2023-09-18 15:14:07 +0530

    Update hitting.py

--- hitting.py
+++ hitting.py
@@ -1,19 +1,16 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 
-# url = "http://127.0.0.1:5000"
-url = "https://api-flight-46lp.onrender.com"
+url = "http://127.0.0.1:5000"
+# url = "https://api-flight-46lp.onrender.com"
 
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/json"
-def hit(prompt,greet,conv1,conv2,last2):
+def hit(prompt,session_id):
     json1={
         "prompt":prompt,
-        "greet":greet,
-        "conv1":conv1,
-        "conv2":conv2,
-        "last2":last2
+        "session_id":session_id,
     }
 
-    resp = requests.post(url, headers=headers,json=json1)
+    resp = requests.post(url, headers=headers,json=json1,allow_redirects=False)
     return(resp.text)
